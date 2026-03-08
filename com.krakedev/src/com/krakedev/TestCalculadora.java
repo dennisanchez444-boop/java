09392a2f46fd56cfed00c9960f5be0fb20f72a6c
dennisanchez444-boop <dennisanchez444@gmail.com>
2026-03-08 19:24:35 -0400

    Invocar las funciones dentro de TestCalculaodra listo

--- com.krakedev/src/com/krakedev/TestCalculadora.java
+++ com.krakedev/src/com/krakedev/TestCalculadora.java
@@ -6,11 +6,21 @@ public class TestCalculadora {
 		Calculadora calcu;
 		int resultadoSuma;
 		int resultadoResta;
+		double resultadoMultiplicacion;
+		double resultadoDividir;
+		double resultadoPromedio;
 		
 		calcu= new Calculadora();
 		resultadoSuma=calcu.sumar(5,3);
         resultadoResta=calcu.restar(10, 7);
+        resultadoMultiplicacion=calcu.multiplicar(10, 5);
+        resultadoDividir=calcu.dividir(10, 2);
+        resultadoPromedio=calcu.promediar(10, 8, 9);
+        calcu.mostrarResultado();
 		System.out.println("RESULTADO SUMA: "+resultadoSuma);
 		System.out.println("RESULTADO RESTA: "+resultadoResta);
+		System.out.println("RESULTADO MULTIPLICACION: "+resultadoMultiplicacion);
+		System.out.println("RESULTADO DIVICION: "+resultadoDividir);
+		System.out.println("RESULTADO PROMEDIO: "+resultadoPromedio);
 	}
 }
