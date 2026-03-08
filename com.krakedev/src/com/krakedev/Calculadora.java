b1f0fb563b498bb77b48b0abc5060cde69482530
dennisanchez444-boop <dennisanchez444@gmail.com>
2026-03-08 19:06:52 -0400

    Funcion dividir lista

--- com.krakedev/src/com/krakedev/Calculadora.java
+++ com.krakedev/src/com/krakedev/Calculadora.java
@@ -18,4 +18,10 @@ public class Calculadora {
 		  resultado= valor1*valor2;
 		  return resultado;
 	  }
+	  
+	  public double dividir(double valor1,double valor2) {
+		  double resultado;
+		  resultado= valor1/valor2;
+		  return resultado;
+	  }
 }
